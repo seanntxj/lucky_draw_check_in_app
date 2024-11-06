b048a6c5c14461b7f2e4d81e3b9e141abc05a6ad
sean.xj.tan.work <sean.xj.tan.work@gmail.com>
2024-11-06 15:38:58 +0800

    Dynamic kinde auth provider based on env

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import App from "./App.tsx";
 import "./index.css";
 import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
+const getRedirectUriBasedOnEnv = () => { 
+  const base = import.meta.env.DEV ? "http://localhost:5173/" : import.meta.env.VITE_GIT_BASE_URL;
+  const git_repo_name = import.meta.env.VITE_GIT_REPO_NAME;
+  console.log(base + git_repo_name + "/")
+  return base + git_repo_name + "/";
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <KindeProvider
-      clientId="b707df7158804fbd818225f1698a0cef"
-      domain="https://sxjtn.kinde.com"
-      redirectUri="https://seanntxj.github.io/lucky_draw_check_in_app/"
-      logoutUri="https://seanntxj.github.io/lucky_draw_check_in_app/"
+      clientId={import.meta.env.VITE_KINDE_CLIENTID}
+      domain={import.meta.env.VITE_KINDE_DOMAIN}
+      redirectUri={getRedirectUriBasedOnEnv()}
+      logoutUri={getRedirectUriBasedOnEnv()}
     >
       <App />
     </KindeProvider>
