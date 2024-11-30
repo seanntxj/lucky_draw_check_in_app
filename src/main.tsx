4e2021206490fcd884afb879e7f94d9ea5e6f822
seanntxj <seanntxj@gmail.com>
2024-11-30 11:21:12 +0800

    Commented out legacy redirect code for Kinde

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const getRedirectUriBasedOnEnv = () => {
-  const base = import.meta.env.DEV
-    ? "http://localhost:5173/"
-    : import.meta.env.VITE_GIT_BASE_URL;
-  const git_repo_name = import.meta.env.VITE_GIT_REPO_NAME;
-  return base + git_repo_name + "/";
-};
+// const getRedirectUriBasedOnEnv = () => {
+//   const base = import.meta.env.DEV
+//     ? "http://localhost:5173/"
+//     : import.meta.env.VITE_GIT_BASE_URL;
+//   const git_repo_name = import.meta.env.VITE_GIT_REPO_NAME;
+//   return base + git_repo_name + "/";
+// };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
